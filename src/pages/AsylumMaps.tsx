15d1d25a7021f4d9faf71d59792c7a26cf83b70a
TheHumphrey <fernandob.delima@outlook.com>
2021-03-26 13:36:35 -0300

    add map api

--- src/pages/AsylumMaps.tsx
+++ src/pages/AsylumMaps.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-
 import { FiPlus } from "react-icons/fi";
-
-import "../styles/pages/asylum-map.css";
+import { Map, TileLayer } from "react-leaflet";
 
 import mapMarkerImg from "../images/map-marker.svg";
+
+import "leaflet/dist/leaflet.css";
+import "../styles/pages/asylum-map.css";
+
 const AsylumMaps = () => {
   return (
     <div id="page-map">
@@ -24,7 +26,16 @@ const AsylumMaps = () => {
           <span>Goiás</span>
         </footer>
       </aside>
-      <div></div>
+      <Map
+        center={[-16.68062, -49.2735347]}
+        zoom={15}
+        style={{ width: "100%", height: "100%" }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      </Map>
 
       <Link to="" className="create-asylum">
         <FiPlus size={32} color="#fff" />
